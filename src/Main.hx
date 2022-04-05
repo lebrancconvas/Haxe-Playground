a35ed481db557282c83320bda82d9d58784a2650
lebrancconvas <lebrancconvas@gmail.com>
2022-04-06 01:00:02 +0700

    ✨ Basic Variable Declaration and Operators.

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,18 @@
 class Main {
 	static function main() {
-		trace("Hello, world!");
+		// Variable 
+		var testVariable:Int = 10;
+		trace(testVariable);
+
+		// Basic Algebric Operators 
+		var member01:Int = 20;
+		var member02:Int = 30;
+		var summember:Int = member01 + member02;
+		var differencemember:Int = member02 - member01;
+		trace(summember); 
+		trace(differencemember);
+		trace(member01 * member02);
+		trace(member02 / member01);
+		trace(member02 % member01);
 	}
 }
