a98bc5e4fc76396d98862a7a288666e9c0ae3327
lebrancconvas <lebrancconvas@gmail.com>
2022-04-05 23:16:59 +0700

    🔧 Build to another language.

---
+++ bin/main.js
@@ -0,0 +1,19 @@
+(function ($global) { "use strict";
+var Main = function() { };
+Main.main = function() {
+	console.log("src/Main.hx:3:","Hello, world!");
+};
+var haxe_iterators_ArrayIterator = function(array) {
+	this.current = 0;
+	this.array = array;
+};
+haxe_iterators_ArrayIterator.prototype = {
+	hasNext: function() {
+		return this.current < this.array.length;
+	}
+	,next: function() {
+		return this.array[this.current++];
+	}
+};
+Main.main();
+})({});
